2d1557f12b09e411c9e3c9a85462b69513f73390
mahdimnkh81 <rezamansourikhah@gmail.com>
2021-07-08 00:28:32 +0430

    class Arrows

---
+++ src/code/card/Arrows.java
@@ -0,0 +1,34 @@
+package code.card;
+
+import java.net.URI;
+
+public class Arrows extends Spell{
+
+    private double radius = 0;
+
+    public Arrows(int cost, URI img, URI sound, double radius) {
+        super(cost, img, sound, radius);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public void playCard() {
+        //    @FXML
+//    void Ball_Shooting(MouseEvent e) {
+//
+//        Circle newCircle = new Circle(e.getX(), e.getY(),fire_ball.getRadius());
+//        drawingAreaPane.getChildren().add(newCircle);
+//        System.out.println(e.getX());
+//        System.out.println(e.getY());
+//    }
+    }
+
+    @Override
+    public void upgradeCard() {
+
+    }
+}
