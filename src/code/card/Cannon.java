79d247d60f981921e2e88c01ec3ef7ea6317197c
mahdimnkh81 <rezamansourikhah@gmail.com>
2021-07-08 00:35:05 +0430

    Class Cannon

--- src/code/card/Cannon.java
+++ src/code/card/Cannon.java
@@ -1,2 +1,20 @@
-package code.card;public class Cannon {
+package code.card;
+
+import java.net.URI;
+
+public abstract class Cannon extends Building implements CardImpl{
+
+
+
+    public Cannon(int cost, URI img, URI sound, int lifetime, int hit_Speed, Target target, Range range) {
+        super(cost, img, sound, lifetime, hit_Speed, target, range);
+    }
+
+
+    @Override
+    public void playCard(){};
+
+    @Override
+    public void upgradeCard(){};
+
 }
