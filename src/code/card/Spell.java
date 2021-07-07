5debb77605d5b44b62fec93a36b83781444c0029
mahdimnkh81 <rezamansourikhah@gmail.com>
2021-07-08 00:39:38 +0430

    Class Spell

--- src/code/card/Spell.java
+++ src/code/card/Spell.java
@@ -3,9 +3,13 @@ package code.card;
 import java.net.URI;
 
 public abstract class  Spell extends AbstractBaseCard implements CardImpl{
-    public Spell(int cost, URI img, URI sound) {
+    private double radius;
+
+    public Spell(int cost, URI img, URI sound , double radius) {
         super(cost, img, sound);
+        this.radius = radius;
     }
+
 
     @Override
     public abstract void playCard();
