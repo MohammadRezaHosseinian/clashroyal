67a177f003d8063237ddfaf5831e92f19ca3e1aa
mahdimnkh81 <rezamansourikhah@gmail.com>
2021-07-08 00:38:27 +0430

    class Inferno_Tower

--- src/code/card/Inferno_Tower.java
+++ src/code/card/Inferno_Tower.java
@@ -1,2 +1,21 @@
-package code.card;public class Inferno_Tower {
+package code.card;
+
+import java.net.URI;
+
+public class Inferno_Tower extends Building implements CardImpl{
+
+
+    public Inferno_Tower(int cost, URI img, URI sound, int lifetime, int hit_Speed, Target target, Range range) {
+        super(cost, img, sound, lifetime, hit_Speed, target, range);
+    }
+
+    @Override
+    public void playCard() {
+
+    }
+
+    @Override
+    public void upgradeCard() {
+
+    }
 }
