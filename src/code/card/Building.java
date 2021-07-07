fe2d169a5bd88955bab5ab971776a53b5e05c398
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-07-07 20:13:47 +0430

    add cards

---
+++ src/code/card/Building.java
@@ -0,0 +1,14 @@
+package code.card;
+
+import java.net.URI;
+
+public class Building extends AbstractBaseCard{
+    public Building(int cost, URI img, URI sound) {
+        super(cost, img, sound);
+    }
+
+    @Override
+    public void playCard() {
+
+    }
+}
