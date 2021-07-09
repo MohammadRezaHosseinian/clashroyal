40c4eb6cab839aad42dc91c5fd4c15370a1d3aa6
mahdimnkh81 <rezamansourikhah@gmail.com>
2021-07-09 18:12:09 +0430

    Fireball upgrade

--- src/code/card/Fire_ball_Upgrade.java
+++ src/code/card/Fire_ball_Upgrade.java
@@ -1,2 +1,32 @@
-package code.card;public class Fire_ball_Upgrade {
+package code.card;
+
+public class Fire_ball_Upgrade extends TroopUpgrade{
+
+    public Fire_ball_Upgrade(Level level) {
+        super(level);
+    }
+
+    @Override
+    public void upgrade() {
+
+        while (true) {
+            switch (level) {
+                case LV1:
+                    this.damage = 325;
+                    break;
+                case LV2:
+                    this.damage = 357;
+                    break;
+                case LV3:
+                    this.damage = 393;
+                    break;
+                case LV4:
+                    this.damage = 432;
+                    break;
+                case LV5:
+                    this.damage = 474;
+                    break;
+            }
+        }
+    }
 }
