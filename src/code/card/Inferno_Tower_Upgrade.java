262c3351004ddf529bdedca585bddd696308fe71
mahdimnkh81 <rezamansourikhah@gmail.com>
2021-07-09 18:13:59 +0430

    Level

--- src/code/card/Inferno_Tower_Upgrade.java
+++ src/code/card/Inferno_Tower_Upgrade.java
@@ -1,2 +1,45 @@
-package code.card;public class Inferno_Tower_Upgrade {
+package code.card;
+
+public class Inferno_Tower_Upgrade extends TroopUpgrade{
+
+    public Inferno_Tower_Upgrade(Level level) {
+        super(level);
+    }
+
+    @Override
+    public void upgrade() {
+
+        while (true) {
+            switch (level) {
+                case LV1:
+                    this.hp = 800;
+                    this.damage = 20;
+                    this.damage2 = 400;
+                    break;
+                case LV2:
+                    hp = 880;
+                    damage = 22;
+                    damage2 = 440;
+                    break;
+                case LV3:
+                    hp = 968;
+                    damage = 24;
+                    damage2 = 484;
+                    break;
+                case LV4:
+                    hp = 1064;
+                    damage = 26;
+                    damage2 = 532;
+                    break;
+                case LV5:
+                    hp = 1168;
+                    damage = 29;
+                    damage2 = 584;
+                    break;
+            }
+        }
+    }
 }
+
+
+
--- src/code/card/Level.java
+++ src/code/card/Level.java
@@ -1,2 +1,9 @@
-package code.card;public class Level {
+package code.card;
+
+public enum Level {
+    LV1,
+    LV2,
+    LV3,
+    LV4,
+    LV5
 }
