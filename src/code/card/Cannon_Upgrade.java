14c115d91d934486a0645d0c0622e35fb86c6ecb
mahdimnkh81 <rezamansourikhah@gmail.com>
2021-07-09 18:11:16 +0430

    Cannon Upgrade

--- src/code/card/Cannon_Upgrade.java
+++ src/code/card/Cannon_Upgrade.java
@@ -1,2 +1,39 @@
-package code.card;public class Cannon_Upgrade {
+package code.card;
+
+import java.net.URI;
+
+public class Cannon_Upgrade extends TroopUpgrade{
+
+    public Cannon_Upgrade(Level level) {
+        super(level);
+    }
+
+    @Override
+    public void upgrade() {
+
+        while (true) {
+            switch (level) {
+                case LV1:
+                    this.hp = 380;
+                    this.damage = 60;
+                    break;
+                case LV2:
+                    hp = 418;
+                    damage = 66;
+                    break;
+                case LV3:
+                    hp = 459;
+                    damage = 72;
+                    break;
+                case LV4:
+                    hp = 505;
+                    damage = 79;
+                    break;
+                case LV5:
+                    hp = 554;
+                    damage = 87;
+                    break;
+            }
+        }
+    }
 }
