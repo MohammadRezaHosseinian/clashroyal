77f936d2f7d926144dac9a9ac0b7b3e69dc56d96
mahdimnkh81 <rezamansourikhah@gmail.com>
2021-07-11 21:36:34 +0430

    controller

--- src/res/fxml/Controller1.java
+++ src/res/fxml/Controller1.java
@@ -1,4 +1,26 @@
 package res.fxml;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller1 {
+
+    @FXML
+    private Button Battle_Deck;
+
+    @FXML
+    private Button Battle_History;
+
+    @FXML
+    private Button Profile;
+
+    @FXML
+    private Button Training_Camp;
+
+    @FXML
+    void Home_Page(ActionEvent event) {
+
+    }
+
 }
