669bda55a9b5341db1cfcd7df441a7e005a3cf68
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-07-18 18:06:17 +0430

    create Troop Hero

---
+++ src/code/heros/BarbariansHero.java
@@ -0,0 +1,62 @@
+package code.heros;
+
+import code.card.Range;
+import code.card.Speed;
+import javafx.scene.image.ImageView;
+
+public class BarbariansHero extends BaseHero implements Fightable, CanMakeSound, CanWalk, DieAble{
+
+    public BarbariansHero(ImageView imageView, int hp, Position pos, Speed speed) {
+        super(imageView, hp, pos, speed);
+    }
+
+    @Override
+    public Position findNextAim() {
+        return null;
+    }
+
+    @Override
+    public void playSound() {
+
+    }
+
+    @Override
+    public void walk() {
+
+    }
+
+    @Override
+    public void die() {
+
+    }
+
+    @Override
+    public void fight() {
+
+    }
+
+    @Override
+    public Fightable getNeartarget() {
+        return null;
+    }
+
+    @Override
+    public Range getRangFight() {
+        return null;
+    }
+
+    @Override
+    public boolean CanAreaSlpash() {
+        return false;
+    }
+
+    @Override
+    public double getHitSpeed() {
+        return 1.5;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
